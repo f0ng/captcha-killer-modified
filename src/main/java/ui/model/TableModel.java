6a74b280fd5075a1e493651bf8ec2e93faeef056
f0ng <jtsqrj@gmail.com>
2022-03-24 10:07:34 +0800

    Update TableModel.java

--- src/main/java/ui/model/TableModel.java
+++ src/main/java/ui/model/TableModel.java
@@ -4,19 +4,18 @@
  */
 package ui.model;
 
+import burp.BurpExtender;
 import entity.CaptchaEntity;
-import java.util.Arrays;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-import javax.xml.bind.DatatypeConverter;
-import ui.GUI;
-
-import javax.swing.*;
-import javax.swing.table.AbstractTableModel;
 import java.util.ArrayList;
 import java.util.List;
-import static utils.Util.base64Encode;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import javax.swing.*;
+import javax.swing.table.AbstractTableModel;
+import javax.xml.bind.DatatypeConverter;
+import ui.GUI;
 import static utils.Util.byte2img;
+import static utils.Util.dataimgToimg;
 
 public class TableModel extends AbstractTableModel {
     private List<String> title = new ArrayList<String>();
@@ -54,28 +53,38 @@ public class TableModel extends AbstractTableModel {
     @Override
     public Object getValueAt(int row, int column) {
         CaptchaEntity captcha = GUI.captcha.get(row);
+        //String words = tfWords
         try {
             switch (column) {
                 case 0:
                     String strr = new String(captcha.getImage());
-                    if (  (strr.contains("data:image") && !strr.startsWith("data:image")) || (strr.contains("data%3Aimage") && !strr.startsWith("data%3Aimage")) ){
-                        String pattern = "(data:image.*?)[\"|&]|(data%2Aimage.*?)[\"|&]|([B|b]ase64\".*)[\"|&]";
-                        Pattern r = Pattern.compile(pattern);
-                        Matcher m = r.matcher(strr);
-                        if (m.find( )) {
-                            strr = m.group(0).replace("\"","").replace("&","").replace("Base64:","").replace("base64:","") ;
-                        }
-                        if (!strr.contains("data:image")){
-                            strr = "data:image/jpeg;base64," + strr;
-                        }
-                        byte[] byteImage = DatatypeConverter.parseBase64Binary(strr.substring(strr.indexOf(",") + 1));
+                    String words = BurpExtender.gui.getTfwords().getText();
+
+                    if (!words.equals("") && strr.contains(words)){
+                        byte[] byteImage = dataimgToimg(new String(captcha.getImage()) ,words);
                         ImageIcon icon = byte2img(byteImage);
                         table.setRowHeight(row, icon.getIconHeight() + 5);//让行高自动适应图片高
                         return icon;
                     }else {
-                        ImageIcon icon = byte2img(captcha.getImage());
-                        table.setRowHeight(row, icon.getIconHeight() + 5);//让行高自动适应图片高
-                        return icon;
+                        if ((strr.contains("data:image") && !strr.startsWith("data:image")) || (strr.contains("data%3Aimage") && !strr.startsWith("data%3Aimage"))) {
+                            String pattern = "(data:image.*?)[\"|&]|(data%2Aimage.*?)[\"|&]";
+                            Pattern r = Pattern.compile(pattern);
+                            Matcher m = r.matcher(strr);
+                            if (m.find()) {
+                                strr = m.group(0).replace("\"", "").replace("&", "").replace("Base64:", "").replace("base64:", "");
+                            }
+                            if (!strr.contains("data:image")) {
+                                strr = "data:image/jpeg;base64," + strr;
+                            }
+                            byte[] byteImage = DatatypeConverter.parseBase64Binary(strr.substring(strr.indexOf(",") + 1));
+                            ImageIcon icon = byte2img(byteImage);
+                            table.setRowHeight(row, icon.getIconHeight() + 5);//让行高自动适应图片高
+                            return icon;
+                        } else {
+                            ImageIcon icon = byte2img(captcha.getImage());
+                            table.setRowHeight(row, icon.getIconHeight() + 5);//让行高自动适应图片高
+                            return icon;
+                        }
                     }
 
                 case 1:
